3420256c29eb000ca012fa4f5e25d7ddcd19c278
Ansel Bridgewater <iamansel@gmail.com>
2018-04-16 02:05:27 -0700

    Fixed tab spacing, added delayed messages, and made the bot initiate chat

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dialogflow = require('dialogflow');
 require('dotenv').config();
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
 
 // Dialogflow variables
@@ -18,8 +18,8 @@ const languageCode = 'en-US';
 const sessionClient = new dialogflow.SessionsClient({
   projectId: projectId,
   credentials: {
-      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.CLIENT_EMAIL
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL
   }
 });
 
@@ -39,13 +39,15 @@ function botResponse(msg) {
     },
   };
 
-  // Return promise that will return bot response
+  // Return promise that will return bot responses
   return sessionClient
     .detectIntent(request)
     .then(responses => {
       console.log('Detected intent');
 
       const result = responses[0].queryResult;
+      console.log(result.fulfillmentMessages[0].text.text[0])
+
       console.log(`  Query: ${result.queryText}`);
       console.log(`  Response: ${result.fulfillmentText}`);
       if (result.intent) {
@@ -54,45 +56,60 @@ function botResponse(msg) {
         console.log(`  No intent matched.`);
       }
 
-      botReply = result.fulfillmentText
-      return botReply
+      botReplies = result.fulfillmentMessages
+      return botReplies
     })
     .catch(err => {
       console.error('ERROR:', err);
     });
 }
 
-// Function for delaying bot response to seem more natural
+// Calculates how long to wait before sending a message
 function botTimeout(msg) {
-    const charactersPerSec = 6
-    const millisecondsInSec = 1000
-    return msg.length / charactersPerSec * millisecondsInSec
+  const charactersPerSec = 6
+  const millisecondsInSec = 1000
+  return msg.length / charactersPerSec * millisecondsInSec
+}
+
+// Sends a message after a calculated delay to seem more natural
+function respondAfterDelay(message, callback) {
+  waitTime = botTimeout(message)
+  setTimeout(function() {
+    callback(message);
+  }, waitTime);
 }
 
 io.on('connection', function (socket) {
-    console.log('a user connected')
+  console.log('a user connected')
+  botResponse('start medication tree').then(replies => {
+    var repliesLength = replies.length;
+    for (var i = 0; i < repliesLength; i++) {
+        io.emit('chat message', replies[i].text.text[0]);
+    }
+  })
 
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-    })
+  socket.on('chat message', function (msg) {
+      io.emit('chat message', msg);
+  })
 
-    socket.on('chat message', function (msg) {
-        // Old Timeout
-        // setTimeout(function () {
-        //   botResponse(msg).then(reply => {
-        //     io.emit('chat message', reply);
-        //   })
-        // }, botTimeout(msg))
-      botResponse(msg).then(reply => {
-        io.emit('chat message', reply);
-      })
-    })
+  socket.on('chat message', function (msg) {
+    botResponse(msg).then(replies => {
+      var repliesLength = replies.length;
+      for (var i = 0; i < repliesLength; i++) {
+        message = replies[i].text.text[0]
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected')
+        respondAfterDelay(message, function(response) {
+          io.emit('chat message', response);
+        })
+      }
     })
+  })
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
 })
 
 http.listen(process.env.PORT || 3000, function(){
-    console.log('Socket Chat listening on port 3000')
+  console.log('Socket Chat listening on port 3000')
 })
